44d170f2818be0b6fe3ccc05a73b6be687087851
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-04 23:23:37 +0530

    Layout Button Updated with link

--- 01taskmanagment/src/app/(auth)/layout.tsx
+++ 01taskmanagment/src/app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
+import Link from "next/link"
 
 type AuthLayoutProps = {
   children: React.ReactNode
@@ -11,9 +12,11 @@ export default function AuthLayout({children}: AuthLayoutProps) {
       <div className="mx-auto max-w-screen-2xl p-4">
         <nav className="flex items-center justify-between">
             <Image src="/logo.svg" alt="logo" width={150} height={100} />
-              <Button variant='secondary'>
-                Sign Up
-              </Button>
+                <Button asChild variant='secondary'>
+              <Link href='/sign-up'>
+                  Sign Up
+              </Link>
+                </Button>
         </nav>
         <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
           {children}
