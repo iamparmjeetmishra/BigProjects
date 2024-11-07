9d84d76456109bd784508e293fe05b4369ab9f9c
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-07 21:47:17 +0530

    Hono Route api added instead of nextjsapi

--- 01taskmanagment/src/app/api/[[...route]]/route.ts
+++ 01taskmanagment/src/app/api/[[...route]]/route.ts
@@ -1,15 +1,19 @@
 import { Hono } from "hono"
 import { handle } from "hono/vercel"
 
+import auth from "@/features/auth/server/route"
+
 const app = new Hono().basePath('/api')
 
 app.get('/health', (c) => {
   return c.text('Health ok')
 })
 
-app.get('/project/:projectId', (c) => {
-  const projectId = c.req.param('projectId')
-  return c.json({ projectId })
-})
+const routes = app
+  .route('/auth', auth)
+
 
 export const GET = handle(app)
+export const POST = handle(app)
+
+export type AppType = typeof routes
