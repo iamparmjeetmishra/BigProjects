ec5d24f3a31e946b49deaf802d626d5bb0ff956e
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-07 21:31:07 +0530

    signin schema email added

--- 01taskmanagment/src/schema/auth-schema.ts
+++ 01taskmanagment/src/schema/auth-schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 
 export const SignInSchema = z.object({
-  email: z.string().min(3, { message: "Email is incorrect" }),
+  email: z.string().email().min(3, { message: "Email is incorrect" }),
   password: z.string().min(6, { message: "Password must be at least 6 characters" }),
 })
 
