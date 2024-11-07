4546bd4c67cd9cb18bfbcc5ceab572c2c34e5d82
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-08 00:59:25 +0530

    new get route to get current user

--- 01taskmanagment/src/features/auth/server/route.ts
+++ 01taskmanagment/src/features/auth/server/route.ts
@@ -5,8 +5,14 @@ import { createAdminClient } from "@/lib/appwrite";
 import { ID } from "node-appwrite";
 import { deleteCookie, setCookie } from "hono/cookie";
 import { AUTH_COOKIE } from "@/lib/constants";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 const app = new Hono()
+	.get("/currentUser", sessionMiddleware, (c) => {
+		const user = c.get("user");
+
+		return c.json({ data: user });
+	})
 	.post("/login", zValidator("json", SignInSchema), async (c) => {
 		const { email, password } = c.req.valid("json");
 
@@ -56,9 +62,14 @@ const app = new Hono()
 
 		return c.json({ success: true, data: user });
 	})
-	.post("/logout", (c) => {
+	.post("/logout", sessionMiddleware, async (c) => {
+		// clear the account
+		const account = c.get("account");
+
 		deleteCookie(c, AUTH_COOKIE);
-		return c.json({success: true})
+		await account.deleteSession("current");
+
+		return c.json({ success: true });
 	});
 
 export default app;
