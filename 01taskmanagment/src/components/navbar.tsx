05d416d91a1580b42b2f25d6f9d8cce3309e5807
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-08 23:31:16 +0530

    navbar

--- 01taskmanagment/src/components/navbar.tsx
+++ 01taskmanagment/src/components/navbar.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import MobileSidbar from './mobile-sidebar'
+import { UserButton } from '@/features/auth/components/user-button'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='flex items-center justify-between pt-4 px-6'>
+      <div className='flex-col hidden lg:flex'> 
+        <h1 className='text-2xl font-semibold'>Home</h1>
+        <p className='text-muted-foreground'>Monitor all of your projects</p>
+      </div>
+      <MobileSidbar />
+      <UserButton />
+    </nav>
   )
 }
