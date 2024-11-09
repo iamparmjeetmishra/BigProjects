f7542fdd985f1ed2db6d18aa50def942ae0ee429
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-09 15:56:55 +0530

    workspace create - error handling and sonner

---
+++ 01taskmanagment/src/features/workspaces/api/use-create-workspace.ts
@@ -0,0 +1,40 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<typeof client.api.workspaces["$post"]>
+
+type RequestType = InferRequestType<typeof client.api.workspaces["$post"]>
+
+
+export const useCreateWorkspace = () => {
+  const router = useRouter()
+  const queryClient = useQueryClient()
+  const mutation = useMutation<
+  ResponseType,
+    Error,
+    RequestType
+  >({
+    mutationFn: async ({json}) => {
+      const response = await client.api.workspaces["$post"]({ json });
+
+      if (!response.ok) {
+        throw new Error(response.statusText || "Failed to create workspace");
+      }
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Workspace created successfully")
+      router.refresh()
+      queryClient.invalidateQueries({queryKey: ["workspaces"]})
+    },
+    onError: (err) => {
+      toast.error(err.message || " Failed to create workspace")
+    }
+  });
+  return mutation;
+}
