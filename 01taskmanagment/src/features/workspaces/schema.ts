ea9be94da8b91b70722ac83de9a1262560aa533c
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-09 15:57:44 +0530

    Workspace infer schema added

--- 01taskmanagment/src/features/workspaces/schema.ts
+++ 01taskmanagment/src/features/workspaces/schema.ts
@@ -3,3 +3,5 @@ import { z } from "zod";
 export const createWorkspaceSchema = z.object({
   name: z.string().trim().min(1, "Required")
 })
+
+export type CreateWorkspaceFormInferSchema = z.infer<typeof createWorkspaceSchema>;
