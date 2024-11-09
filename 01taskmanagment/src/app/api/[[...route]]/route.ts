aaac10e96e062d02928c9a8a907ab275a154bcda
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-09 15:10:40 +0530

    Main route updated with workspace

--- 01taskmanagment/src/app/api/[[...route]]/route.ts
+++ 01taskmanagment/src/app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono"
 import { handle } from "hono/vercel"
 
 import auth from "@/features/auth/server/route"
+import workspaces from "@/features/workspaces/server/route"
 
 const app = new Hono().basePath('/api')
 
@@ -11,6 +12,7 @@ app.get('/health', (c) => {
 
 const routes = app
   .route('/auth', auth)
+  .route('/workspaces', workspaces)
 
 
 export const GET = handle(app)
