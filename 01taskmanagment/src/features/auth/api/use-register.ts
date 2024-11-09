1762ad3dacfaef91b099267ef68731235f420583
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-09 15:58:03 +0530

    Better error handling + toaster

--- 01taskmanagment/src/features/auth/api/use-register.ts
+++ 01taskmanagment/src/features/auth/api/use-register.ts
@@ -1,7 +1,9 @@
-import { useMutation } from "@tanstack/react-query";
+import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<typeof client.api.auth.register["$post"]>
 
@@ -9,6 +11,8 @@ type RequestType = InferRequestType<typeof client.api.auth.register["$post"]>
 
 
 export const useRegister = () => {
+  const router = useRouter()
+  const queryClient = useQueryClient()
   const mutation = useMutation<
     ResponseType,
     Error,
@@ -16,7 +20,20 @@ export const useRegister = () => {
   >({
     mutationFn: async ({json}) => {
       const response = await client.api.auth.register["$post"]({ json });
+
+      if (!response.ok) {
+        throw new Error(response.statusText || "Failed to register");
+      }
+      
       return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Registered successfully")
+      router.refresh()
+      queryClient.invalidateQueries({ queryKey: ["currentUser"] })
+    },
+    onError: (error) => {
+      toast.error(error.message || "Failed to register user")
     }
   });
   return mutation;
