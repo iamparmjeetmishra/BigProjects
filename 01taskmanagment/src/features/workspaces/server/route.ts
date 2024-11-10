96fa2fda1499bd2277805542527cd2bae4bf3f77
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-10 23:42:32 +0530

    Workspace Image handling route added

--- 01taskmanagment/src/features/workspaces/server/route.ts
+++ 01taskmanagment/src/features/workspaces/server/route.ts
@@ -2,7 +2,7 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schema";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, WORKSPACES_ID } from "@/lib/constants";
+import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/lib/constants";
 import { ID } from "node-appwrite"
 
 const app = new Hono()
@@ -13,7 +13,29 @@ const app = new Hono()
     async (c) => {
       const databases = c.get("databases");
       const user = c.get("user");
-      const { name } = c.req.valid("json");
+      const storage = c.get("storage");
+
+      const { name, image } = c.req.valid("json");
+
+      let uploadedImageUrl: string | undefined
+      
+      //handling images
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image
+        );
+        // we have to create image buffer from base64 string
+
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id
+        )
+
+        // Into Base64 sting
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`
+      }
 
       const workspace = await databases.createDocument(
         DATABASE_ID,
@@ -21,7 +43,8 @@ const app = new Hono()
         ID.unique(),
         {
           name,
-          userId: user.$id
+          userId: user.$id,
+          imageUrl: uploadedImageUrl
         }
       )
       return c.json({data: workspace})
