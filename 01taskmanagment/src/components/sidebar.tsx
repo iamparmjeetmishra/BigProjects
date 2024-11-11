f8c4b046ddf7f7bf52fbc69d7fa63ae2816ad8d6
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-11 23:05:21 +0530

    sidebar updated for Workspaces

--- 01taskmanagment/src/components/sidebar.tsx
+++ 01taskmanagment/src/components/sidebar.tsx
@@ -1,11 +1,14 @@
 import DottedSeparator from "./dotted-separator";
 import Logo from "./logo";
 import Navigation from "./navigation";
+import WorkspaceSwithcer from "./workspace-switcher";
 
 export default function Sidebar() {
   return (
     <aside className="h-full bg-neutral-100 p-4 w-full">
       <Logo />
+      <DottedSeparator className="my-4" />
+      <WorkspaceSwithcer />
       <DottedSeparator className="my-4" />
       <Navigation />
     </aside>
