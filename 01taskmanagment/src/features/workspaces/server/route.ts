0decbe0221410a7ef3d26ed475d2cae1bed9c702
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-11 23:04:31 +0530

    Route fixed to upload currect encoding for svg file

--- 01taskmanagment/src/features/workspaces/server/route.ts
+++ 01taskmanagment/src/features/workspaces/server/route.ts
@@ -2,53 +2,76 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schema";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/lib/constants";
-import { ID } from "node-appwrite"
+import {
+	DATABASE_ID,
+	IMAGES_BUCKET_ID,
+	WORKSPACES_ID,
+} from "@/lib/constants";
+import { ID } from "node-appwrite";
 
 const app = new Hono()
-  .post(
-    "/",
-    zValidator("form", createWorkspaceSchema),
-    sessionMiddleware,
-    async (c) => {
-      const databases = c.get("databases");
-      const user = c.get("user");
-      const storage = c.get("storage");
+	.get("/", sessionMiddleware, async (c) => {
+		const databases = c.get("databases");
 
-      const { name, image } = c.req.valid("form");
+		const workspaces = await databases.listDocuments(
+			DATABASE_ID,
+			WORKSPACES_ID
+		);
 
-      let uploadedImageUrl: string | undefined
-      
-      //handling images
-      if (image instanceof File) {
-        const file = await storage.createFile(
-          IMAGES_BUCKET_ID,
-          ID.unique(),
-          image
+		return c.json({ data: workspaces });
+	})
+	.post(
+		"/",
+		zValidator("form", createWorkspaceSchema),
+		sessionMiddleware,
+		async (c) => {
+			const databases = c.get("databases");
+			const user = c.get("user");
+			const storage = c.get("storage");
+
+			const { name, image } = c.req.valid("form");
+
+			let uploadedImageUrl: string | undefined;
+
+			//handling images
+			if (image instanceof File) {
+				const file = await storage.createFile(
+					IMAGES_BUCKET_ID,
+					ID.unique(),
+					image
+				);
+				// we have to create image buffer from base64 string
+
+				const arrayBuffer = await storage.getFileView(
+					IMAGES_BUCKET_ID,
+					file.$id
         );
-        // we have to create image buffer from base64 string
+        console.log(arrayBuffer);
 
-        const arrayBuffer = await storage.getFilePreview(
-          IMAGES_BUCKET_ID,
-          file.$id
-        )
 
-        // Into Base64 sting
-        uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`
-      }
+				// Into Base64 sting
+				if (image.type === "image/svg+xml") {
+          // converting utf-8 string for svg
+          const svgString = Buffer.from(arrayBuffer).toString("utf-8");
+					uploadedImageUrl = `data:image/svg+xml,${encodeURIComponent(svgString)}`;
+				} else {
+          const imgString = Buffer.from(arrayBuffer).toString("base64")
+          uploadedImageUrl = `data:image/png;base64,${imgString}`;
+				}
+			}
 
-      const workspace = await databases.createDocument(
-        DATABASE_ID,
-        WORKSPACES_ID,
-        ID.unique(),
-        {
-          name,
-          userId: user.$id,
-          imageUrl: uploadedImageUrl
-        }
-      )
-      return c.json({data: workspace})
-    }
-  )
+			const workspace = await databases.createDocument(
+				DATABASE_ID,
+				WORKSPACES_ID,
+				ID.unique(),
+				{
+					name,
+					userId: user.$id,
+					imageUrl: uploadedImageUrl,
+				}
+			);
+			return c.json({ data: workspace });
+		}
+	);
 
 export default app;
