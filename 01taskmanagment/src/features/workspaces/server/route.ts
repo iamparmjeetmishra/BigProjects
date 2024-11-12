26370a38109097f02195c63a490a38d2290abd5a
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-12 23:41:16 +0530

    Route to handle member filter

--- 01taskmanagment/src/features/workspaces/server/route.ts
+++ 01taskmanagment/src/features/workspaces/server/route.ts
@@ -4,18 +4,41 @@ import { createWorkspaceSchema } from "../schema";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import {
 	DATABASE_ID,
+	GenerateInviteCodeLength,
 	IMAGES_BUCKET_ID,
+	MEMBERS_ID,
 	WORKSPACES_ID,
 } from "@/lib/constants";
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
+import { MemberRole } from "@/features/members/types";
+import { GenerateInviteCode } from "@/lib/utils";
 
 const app = new Hono()
 	.get("/", sessionMiddleware, async (c) => {
+		const user = c.get("user");
 		const databases = c.get("databases");
+
+		const members = await databases.listDocuments(
+			DATABASE_ID,
+			MEMBERS_ID,
+			[Query.equal("userId", user.$id)]
+		);
+
+		if (members.total === 0) {
+			return c.json({ data: { documents: [], total: 0 } });
+		}
+
+		const workspaceIds = members.documents.map(
+			(member) => member.workspaceId
+		);
 
 		const workspaces = await databases.listDocuments(
 			DATABASE_ID,
-			WORKSPACES_ID
+			WORKSPACES_ID,
+			[
+				Query.orderDesc("createdAt"),
+				Query.contains("$id", workspaceIds),
+			]
 		);
 
 		return c.json({ data: workspaces });
@@ -45,18 +68,19 @@ const app = new Hono()
 				const arrayBuffer = await storage.getFileView(
 					IMAGES_BUCKET_ID,
 					file.$id
-        );
-        console.log(arrayBuffer);
-
+				);
+				console.log(arrayBuffer);
 
 				// Into Base64 sting
 				if (image.type === "image/svg+xml") {
-          // converting utf-8 string for svg
-          const svgString = Buffer.from(arrayBuffer).toString("utf-8");
+					// converting utf-8 string for svg
+					const svgString =
+						Buffer.from(arrayBuffer).toString("utf-8");
 					uploadedImageUrl = `data:image/svg+xml,${encodeURIComponent(svgString)}`;
 				} else {
-          const imgString = Buffer.from(arrayBuffer).toString("base64")
-          uploadedImageUrl = `data:image/png;base64,${imgString}`;
+					const imgString =
+						Buffer.from(arrayBuffer).toString("base64");
+					uploadedImageUrl = `data:image/png;base64,${imgString}`;
 				}
 			}
 
@@ -68,8 +92,21 @@ const app = new Hono()
 					name,
 					userId: user.$id,
 					imageUrl: uploadedImageUrl,
+					inviteCode: GenerateInviteCode(GenerateInviteCodeLength),
 				}
 			);
+
+			await databases.createDocument(
+				DATABASE_ID,
+				MEMBERS_ID,
+				ID.unique(),
+				{
+					userId: user.$id,
+					workspaceId: workspace.$id,
+					role: MemberRole["ADMIN"],
+				}
+			);
+
 			return c.json({ data: workspace });
 		}
 	);
