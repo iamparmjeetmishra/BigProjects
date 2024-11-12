816955b91cd1562448946440dfe0302fb9350da2
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-12 23:40:05 +0530

    Random Pin generator

--- 01taskmanagment/src/lib/utils.ts
+++ 01taskmanagment/src/lib/utils.ts
@@ -9,3 +9,13 @@ export function cn(...inputs: ClassValue[]) {
 export function CapitalizeIndex(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+export function GenerateInviteCode(length: number) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = ""
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+}
