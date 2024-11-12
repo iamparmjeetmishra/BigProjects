7e6f6b02c61cc0d4f6580116ac315e43b8073bf6
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-12 23:40:29 +0530

    members id

--- 01taskmanagment/src/lib/constants.ts
+++ 01taskmanagment/src/lib/constants.ts
@@ -5,4 +5,7 @@ export const NEXT_PUBLIC_APPWRITE_PROJECT = process.env.NEXT_PUBLIC_APPWRITE_PRO
 export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!
 export const AUTH_COOKIE = "cwa-jira-clone-session"
 export const WORKSPACES_ID = process.env.NEXT_PUBLIC_APPWRITE_WORKSPACES_ID!
-export const IMAGES_BUCKET_ID=process.env.NEXT_PUBLIC_IMAGES_BUCKET_ID!
+export const IMAGES_BUCKET_ID = process.env.NEXT_PUBLIC_IMAGES_BUCKET_ID!
+export const MEMBERS_ID = process.env.NEXT_PUBLIC_APPWRITE_MEMBERS_ID!
+
+export const GenerateInviteCodeLength = 6
